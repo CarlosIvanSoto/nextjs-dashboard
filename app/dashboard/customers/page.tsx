4fba2bbfb76a252843d6b61e622d2fca9d5b7696
Carlos Ivan Soto Perez <gameue@hotmail.com>
2024-02-07 14:02:48 -0600

    corrections

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,21 @@
-export default function Page () {
-  return <p>Customers Page</p>
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+import Table from '@/app/ui/customers/table';
+import { lusitana } from "@/app/ui/fonts"
+import Search from '@/app/ui/search';
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'Customers',
+}
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || ''
+  return <Table query={query} />
 }
